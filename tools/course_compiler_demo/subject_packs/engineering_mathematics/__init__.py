5ed633aa6e3113ad8a1ddf28b89cd74c56c4c6cb
Melvin Richardson <mpr.bap.dev@gmail.com>
2026-07-30 13:06:11 -0700

    Add engineering mathematics course packs

---
+++ tools/course_compiler_demo/subject_packs/engineering_mathematics/__init__.py
@@ -0,0 +1,6 @@
+"""Engineering mathematics course catalog."""
+from .packs import (COURSE_IDS, ENGINE_ALLOCATIONS, build_engineering_mathematics_catalog,
+    validate_engineering_mathematics_catalog)
+
+__all__=["COURSE_IDS","ENGINE_ALLOCATIONS","build_engineering_mathematics_catalog",
+    "validate_engineering_mathematics_catalog"]
